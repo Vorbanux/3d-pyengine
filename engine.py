ed66470f9d934f1e61397af963940f18e44106cd
Vorbanux <bobylevv472@gmail.com>
2026-06-07 00:07:33 +0500

    update

--- engine.py
+++ engine.py
@@ -10,6 +10,7 @@ class point:
             [x], [y], [z], [1.0]
         ], dtype=np.float32)
         self.rgba = color
+
     @property
     def x(self) -> float:
         return self.pos[0, 0]
@@ -109,71 +110,7 @@ class camera(point):
         
         self.transform = self.rotation @ self.position
 
-    def projection(self, data_point: np.ndarray, vertices: np.ndarray, screen_size: list) -> list:
-        self._update_matrices()
-        
-        f = 1 / np.tan(np.radians(self.angle)/2)
-        aspect = screen_size[0] / screen_size[1]
-        project = np.array([
-                [-f/aspect, 0.0, 0.0, 0.0],
-                [0.0, f, 0.0, 0.0],
-                [0.0, 0.0, (FAR_PLANE+NEAR_PLANE)/(NEAR_PLANE-FAR_PLANE), (2*FAR_PLANE*NEAR_PLANE)/(NEAR_PLANE-FAR_PLANE)],
-                [0.0, 0.0, -1.0, 0.0]
-            ], dtype=np.float32)
-        mvp = project @ self.transform
-        
-        clip_points = (mvp @ vertices.T).T
-        clip_points[:, 3] = -clip_points[:, 3]
-        
-        idx_start = data_point[:, 0]
-        idx_end   = data_point[:, 1]
-        
-        p1 = clip_points[idx_start]
-        p2 = clip_points[idx_end]
-
-        w1 = p1[:, 3]
-        w2 = p2[:, 3]
-
-        both_behind = (w1 < NEAR_PLANE) & (w2 < NEAR_PLANE)
-        both_too_far = (w1 > FAR_PLANE) & (w2 > FAR_PLANE)
-        culled_mask = both_behind | both_too_far
-
-        valid_indices = np.where(~culled_mask)[0]
-        p1_valid = p1[valid_indices].copy()
-        p2_valid = p2[valid_indices].copy()
-        w1_valid = w1[valid_indices]
-        w2_valid = w2[valid_indices]
-
-        t1 = (NEAR_PLANE - w1_valid) / (w2_valid - w1_valid + 1e-6)
-        mask1 = (w1_valid < NEAR_PLANE) & (w2_valid >= NEAR_PLANE)
-        p1_valid[mask1] = p1_valid[mask1] + t1[mask1, np.newaxis] * (p2_valid[mask1] - p1_valid[mask1])
-        
-        t2 = (NEAR_PLANE - w2_valid) / (w1_valid - w2_valid + 1e-6)
-        mask2 = (w2_valid < NEAR_PLANE) & (w1_valid >= NEAR_PLANE)
-        p2_valid[mask2] = p2_valid[mask2] + t2[mask2, np.newaxis] * (p1_valid[mask2] - p2_valid[mask2])
-        
-        t3 = (FAR_PLANE - w1_valid) / (w2_valid - w1_valid + 1e-6)
-        mask3 = (w1_valid > FAR_PLANE) & (w2_valid <= FAR_PLANE)
-        p1_valid[mask3] = p1_valid[mask3] + t3[mask3, np.newaxis] * (p2_valid[mask3] - p1_valid[mask3])
-        
-        t4 = (FAR_PLANE - w2_valid) / (w1_valid - w2_valid + 1e-6)
-        mask4 = (w2_valid < FAR_PLANE) & (w1_valid <= FAR_PLANE)
-        p2_valid[mask4] = p2_valid[mask4] + t4[mask4, np.newaxis] * (p1_valid[mask4] - p2_valid[mask4])
-
-        p1_ndc = p1_valid[:, :2] / p1_valid[:, 3:4]
-        p2_ndc = p2_valid[:, :2] / p2_valid[:, 3:4]
-        
-        width, height = screen_size[0], screen_size[1]
-        scr_x1 = ((p1_ndc[:, 0] + 1.0) * width / 2.0).astype(np.int32)
-        scr_y1 = ((-p1_ndc[:, 1] + 1.0) * height / 2.0).astype(np.int32)
-        scr_x2 = ((p2_ndc[:, 0] + 1.0) * width / 2.0).astype(np.int32)
-        scr_y2 = ((-p2_ndc[:, 1] + 1.0) * height / 2.0).astype(np.int32)
-        
-        lines_start = np.column_stack((scr_x1, scr_y1))
-        lines_end = np.column_stack((scr_x2, scr_y2))
-        return lines_start, lines_end, valid_indices
-
-    def projection_faces(self, data_point: np.ndarray, vertices: np.ndarray, screen_size: list):
+    def projection(self, data_point: np.ndarray, vertices: np.ndarray, screen_size: list):
         self._update_matrices()
         
         f = 1 / np.tan(np.radians(self.angle)/2)
@@ -211,3 +148,24 @@ class camera(point):
             avg_depths.append(np.mean(clipped_verts[:, 3]))
             
         return final_polygons, valid_indices, np.array(avg_depths, dtype=np.float32)
+    
+    @property
+    def x(self) -> float:
+        return self.pos[0, 0]
+    
+    @property
+    def y(self) -> float:
+        return self.pos[1, 0]
+
+    @property
+    def z(self) -> float:
+        return self.pos[2, 0]
+    
+    @x.setter
+    def x(self, value: float): self.pos[0, 0] = value
+        
+    @y.setter
+    def y(self, value: float): self.pos[1, 0] = value
+        
+    @z.setter
+    def z(self, value: float): self.pos[2, 0] = value
--- grafics.py
+++ grafics.py
@@ -2,19 +2,33 @@ import pygame
 import engine as en
 import numpy as n
 import sys
+import math
 
 base_color = None
 screen = None
 cam = None
 vertices = n.empty((0, 4), dtype=n.float32)
-edges = n.empty((0, 2), dtype=n.int32)
 faces = []
 faces_colors = n.empty((0, 4), dtype=n.int32)
-colors = n.empty((0, 4), dtype=n.int32)
 faces_lines = n.empty((0, 5), dtype=n.int32)
+
+def world_location(world_primary: list, tolist: bool, local_points: list) -> n.ndarray or list:
+    world_points = n.array(local_points, dtype=n.float32)
+    primary_matrix = n.ones((len(local_points), 3), dtype=n.float32) * world_primary
+    world_points[:, :3] += primary_matrix[:, :3]
+    if tolist: world_points = world_points.tolist()
+    return world_points
+
+def local_location(world_primary: list, tolist: bool, *world_points: list) -> n.ndarray or list:
+    local_points = n.array(world_points, dtype=n.float32)
+    primary_matrix = n.ones((len(world_points), 3), dtype=n.float32) * world_primary
+    local_points[:, :3] -= primary_matrix[:, :3]
+    if tolist: local_points = local_points.tolist()
+    return local_points
 
 def create_window(width: int, height: int, bg_color: list, cam_x: float, cam_y: float, cam_z: float, cam_angle: float = 70):
     global base_color, screen, cam
+    pygame.font.init()
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption("My 3D Engine")
     base_color = bg_color
@@ -23,96 +37,153 @@ def create_window(width: int, height: int, bg_color: list, cam_x: float, cam_y: 
     pygame.mouse.set_visible(False)
     pygame.event.set_grab(True)
 
-def edge(pos_primary: list, pos_end: list, color: list):
-    global vertices, edges, colors
-    start = n.array([[float(pos_primary[0]), float(pos_primary[1]), float(pos_primary[2]), 1.0]], dtype=n.float32)
-    end = n.array([[float(pos_end[0]), float(pos_end[1]), float(pos_end[2]), 1.0]], dtype=n.float32)
-    pos = vertices.shape[0]
-    vertices = n.vstack((vertices, start))
-    vertices = n.vstack((vertices, end))
-    edges = n.vstack((edges, n.array([[pos, pos + 1]], dtype=n.int32)))
-    colors = n.vstack((colors, n.array([[color[0], color[1], color[2], color[3]]], dtype=n.int32)))
+class face:
+    def __init__(self, color: list, line_color: list, line_width: int, primary: list, world_poses: list = None, local_poses: list = None):
+        global vertices, faces, faces_colors, faces_lines
+        
+        self.color = color
+        self.line_color = line_color
+        self.line_width = line_width
+        self.primary = primary
+        
+        self.rotation = n.array([[0.0], [0.0], [0.0]], dtype=n.float32)
+        
+        if world_poses:
+            self.world_poses = world_poses
+            self.local_poses = local_location(primary, True, world_poses)[0]
+        elif local_poses:
 
-def face(color: list, line_color: list, line_width: int, poses: list):
-    global vertices, faces, faces_colors, faces_lines
-    n_points = len(poses)
-    pos = vertices.shape[0]
-    
-    new_verts = n.array([[float(p[0]), float(p[1]), float(p[2]), 1.0] for p in poses], dtype=n.float32)
-    vertices = n.vstack((vertices, new_verts))
-    
-    poly_indices = list(range(pos, pos + n_points))
-    
-    faces.append(poly_indices)
-    
-    f_alpha = color[3] if len(color) > 3 else 255
-    new_color = n.array([[color[0], color[1], color[2], f_alpha]], dtype=n.int32)
-    faces_colors = n.vstack((faces_colors, new_color))
-    
-    l_alpha = line_color[3] if len(line_color) > 3 else 255
-    
-    faces_lines = n.vstack((faces_lines, n.array([[int(line_color[0]), int(line_color[1]), int(line_color[2]), int(l_alpha), int(line_width)]], dtype=n.int32)))
+            self.world_poses = world_location(primary, True, local_poses)[0]
+            self.local_poses = local_poses
+        else:
+            raise ValueError("error: world_poses or local_poses must contain value: list")
+        
+        poses = list(self.world_poses)[::-1]
+        poses.append(primary)
+        poses = poses[::-1]
+        
+        n_points = len(poses)
+        pos = vertices.shape[0]
+        
+        new_verts = n.array([[float(p[0]), float(p[1]), float(p[2]), 1.0] for p in poses], dtype=n.float32)
+        vertices = n.vstack((vertices, new_verts))
+        
+        poly_indices = list(range(pos, pos + n_points))
+        
+        faces.append(poly_indices)
+        
+        f_alpha = color[3] if len(color) > 3 else 255
+        new_color = n.array([[color[0], color[1], color[2], f_alpha]], dtype=n.int32)
+        faces_colors = n.vstack((faces_colors, new_color))
+        
+        l_alpha = line_color[3] if len(line_color) > 3 else 255
+        
+        faces_lines = n.vstack((faces_lines, n.array([[int(line_color[0]), int(line_color[1]), int(line_color[2]), int(l_alpha), int(line_width)]], dtype=n.int32)))
 
-def cube(line_color: list, line_width: float, color: list, pos_primary: list, pos_B: list, pos_C: list, pos_D: list, pos_A1: list, pos_B1: list, pos_C1: list, pos_D1: list):
-    face(color, line_color, line_width, [pos_primary, pos_B, pos_C, pos_D])
-    face(color, line_color, line_width, [pos_C, pos_D, pos_D1, pos_C1])
-    face(color, line_color, line_width, [pos_D, pos_primary, pos_A1, pos_D1])
-    face(color, line_color, line_width, [pos_primary, pos_B, pos_B1, pos_A1])
-    face(color, line_color, line_width, [pos_B, pos_C, pos_C1, pos_B1])
-    face(color, line_color, line_width, [pos_A1, pos_B1, pos_C1, pos_D1])
+    def location(self): 
+        self.world_poses = world_location(self.primary, True, self.local_poses)
+
+    @property
+    def get_location(self) -> list: return self.primary
+    
+    @get_location.setter
+    def get_location(self, value: list): 
+        self.primary = value
+        self.location()
+
+class edge:
+    def __init__(self, color: list, width: list, primary: list, world_end: list = None, local_end: list = None):
+        self.color = color
+        self.width = width
+        self.primary = primary
+        
+        if world_end:
+            self.world_end = world_end
+            self.local_end = local_location(primary, True, [world_end])
+        elif local_end:
+            self.world_end = world_location(primary, True, [local_end])
+            self.local_end = local_end
+        else:
+            raise ValueError("error: world_poses or local_poses must contain value: list")
+        
+        face((0, 0, 0, 0), color, width, primary, self.world_end)
+    
+    def location(self): 
+        self.world_end = world_location(self.primary, True, self.local_end)
+
+    @property
+    def get_location(self) -> list: return self.primary
+    
+    @get_location.setter
+    def get_location(self, value: list): 
+        self.primary = value
+        self.location()
+
+
+class cube:
+    def __init__(self, color: list, line_color: list, line_width: int, primary: list, world_poses: list = None, local_poses: list = None):
+        self.color = color
+        self.line_color = line_color
+        self.line_width = line_width
+        self.primary = primary
+        
+        if world_poses:
+            self.world_poses = world_poses
+            self.local_poses = local_location(primary, True, world_poses)
+        elif local_poses:
+
+            self.world_poses = world_location(primary, True, local_poses)
+            self.local_poses = local_poses
+        else:
+            raise ValueError("error: world_poses or local_poses must contain value: list")
+        
+        poses = list(self.world_poses)[::-1]
+        poses.append(primary)
+        poses = poses[::-1]
+        
+        cube_topology = [
+            [0, 1, 2, 3],  # Down
+            [2, 3, 7, 6],  # Back
+            [3, 0, 4, 7],  # Left
+            [0, 1, 5, 4],  # Forward
+            [1, 2, 6, 5],  # Right
+            [4, 5, 6, 7]   # Up
+        ]
+
+        p = poses
+        for face_indices in cube_topology:
+
+            face_verts = [p[idx] for idx in face_indices]
+            face(
+                color, 
+                line_color, 
+                line_width, 
+                primary=face_verts[0], 
+                world_poses=face_verts[1:]
+            )
+    
+    def location(self): 
+        self.world_poses = world_location(self.primary, True, self.local_poses)
+
+    @property
+    def get_location(self) -> list: return self.primary
+    
+    @get_location.setter
+    def get_location(self, value: list): 
+        self.primary = value
+        self.location()
+
 
 def pyramid(line_color: list, line_width: float, color: list, pos_primary: list, poses: list):
-    global vertices, faces, faces_colors, faces_lines
-
-    poses = list(poses)
-    num_base_points = len(poses)
-
-    all_verts_list = poses[::-1]
-    all_verts_list.append(pos_primary)
-    all_verts_list = all_verts_list[::-1]
-    new_verts = n.array([[float(p[0]), float(p[1]), float(p[2]), 1.0] for p in all_verts_list], dtype=n.float32)
-
-    base_idx = vertices.shape[0]
-    vertices = n.vstack((vertices, new_verts))
-
-    base_indices = list(range(base_idx + 1, base_idx + 1 + num_base_points))
-    faces.append(base_indices)
-
-    f_alpha = color[3] if len(color) > 3 else 255
-    faces_colors = n.vstack((faces_colors, n.array([[color[0], color[1], color[2], f_alpha]], dtype=n.int32)))
-    l_alpha = line_color[3] if len(line_color) > 3 else 255
-    line_data = n.array([[int(line_color[0]), int(line_color[1]), int(line_color[2]), int(l_alpha), int(line_width)]], dtype=n.int32)
-    faces_lines = n.vstack((faces_lines, line_data))
-
-    for p in range(num_base_points):
-
-        current_base_local = p + 1
-        next_base_local = ((p + 1) % num_base_points) + 1
-
-        v_current = base_idx + current_base_local
-        v_next = base_idx + next_base_local
-        v_peak = base_idx
-
-        faces.append([v_next, v_current, v_peak])
-
-        faces_colors = n.vstack((faces_colors, n.array([[color[0], color[1], color[2], f_alpha]], dtype=n.int32)))
-        faces_lines = n.vstack((faces_lines, line_data))
+    face(color, line_color, line_width, poses[0], poses[1:])
+    for p in range(len(poses)):
+        next = (p+1) % len(poses)
+        face(color, line_color, line_width, poses[p], (poses[next], pos_primary))
 
 
 def update():
     global vertices, edges, colors, faces, faces_colors, faces_lines
-    result = cam.projection(edges, vertices, [screen.get_width(), screen.get_height()])
-    if result is not None:
-        lines_start, lines_end, valid_indices = result
-        if isinstance(valid_indices, tuple):
-            valid_indices = valid_indices[0]
-        for i in range(len(lines_start)):
-            p1 = tuple(lines_start[i])
-            p2 = tuple(lines_end[i])
-            original_edge_idx = valid_indices[i]
-            color_rgb = tuple(colors[original_edge_idx, :3])
-            pygame.draw.line(screen, color_rgb, p1, p2, 2)
-    result = cam.projection_faces(faces, vertices, [screen.get_width(), screen.get_height()])
+    result = cam.projection(faces, vertices, [screen.get_width(), screen.get_height()])
     if result is not None:
         final, valid, avg = result
         if isinstance(valid, tuple):
@@ -131,6 +202,10 @@ def update():
             )
             l_width = int(faces_lines[original_face_idx, 4])
             pygame.draw.polygon(screen, l_color_rgb, final[i], l_width)
+    
+        text_surface = pygame.font.SysFont(None, 24).render(f"x: {math.floor(cam.pos[0, 0]*100)/100}; y: {math.floor(cam.pos[1, 0]*100)/100}; z: {math.floor(cam.pos[2, 0]*100)/100}", True, (255, 0, 0, 1))
+
+        screen.blit(text_surface, (100, 100))
         return True
 
 def start():
